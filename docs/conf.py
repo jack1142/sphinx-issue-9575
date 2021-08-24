e2dbaeb5e5c4bbf694de636e86e5ab51d271baf7
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-08-24 14:14:05 +0200

    Add necessary Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -54,3 +54,5 @@ html_static_path = ['_static']
 
 
 # -- Extension configuration -------------------------------------------------
+
+autodoc_typehints = "description"
